25799f80e631ce4c4d62380adb9768e635cdfa02
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-09-12 22:51:24 +0530

    Time: 3 ms (84.03%), Space: 9.8 MB (50.51%) - LeetHub

--- 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.cpp
@@ -13,30 +13,42 @@ class Solution {
 public:
 int maxDiffi;
 
-void findMaxDiffUtil(TreeNode* root,TreeNode*child){
-    if(root==NULL||child==NULL){
-        return;
-    }
-    maxDiffi=max(maxDiffi,abs(root->val - child->val));
-    findMaxDiffUtil(root,child->left);
-    findMaxDiffUtil(root,child->right);
+// void findMaxDiffUtil(TreeNode* root,TreeNode*child){
+//     if(root==NULL||child==NULL){
+//         return;
+//     }
+//     maxDiffi=max(maxDiffi,abs(root->val - child->val));
+//     findMaxDiffUtil(root,child->left);
+//     findMaxDiffUtil(root,child->right);
+// }
+
+// void findMaxDiff(TreeNode* root){
+//     if(root==NULL){
+//         return;
+//     }
+//     findMaxDiffUtil(root,root->left);
+//     findMaxDiffUtil(root,root->right);
+    
+//     findMaxDiff(root->left);
+//     findMaxDiff(root->right);
+    
+// }
+int findMaxDiff(TreeNode* root,int minV,int maxV){
+if(root==NULL){
+    return abs(minV-maxV);
+}
+minV=min(minV,root->val);
+maxV=max(maxV,root->val);
+
+int l=findMaxDiff(root->left,minV,maxV);
+int r=findMaxDiff(root->right,minV,maxV);
+
+return max(l,r);
 }
 
-void findMaxDiff(TreeNode* root){
-    if(root==NULL){
-        return;
-    }
-    findMaxDiffUtil(root,root->left);
-    findMaxDiffUtil(root,root->right);
-    
-    findMaxDiff(root->left);
-    findMaxDiff(root->right);
-    
-}
+
     int maxAncestorDiff(TreeNode* root) {
-         maxDiffi=-1;
-    findMaxDiff(root);
+        return findMaxDiff(root,root->val,root->val);
     
-    return maxDiffi;
     }
 };
