ae309961133a37d25bb259b5dd7b4274fbd8ef07
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-09-22 02:23:11 +0530

    Time: 30 ms (42.39%), Space: 89.6 MB (47.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,70 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size();
-        int n=nums2.size();
-
-       int size=m+n;
-
-       int idx1=(size/2)-1;
-       int element1=-1;
-
-       int idx2=(size/2);
-       int element2=-1;
-
-        int i=0;
-        int j=0;
-
-        int k=0;
-
-        while(i<m&&j<n){
-            if(nums1[i]<nums2[j]){
-                    if(k==idx1){
-                        element1=nums1[i];
-                    }
-                    if(k==idx2){
-                        element2=nums1[i];
-                    }
-                    i++;
-            }else{
-                    if(k==idx1){
-                    element1=nums2[j];
-                    }
-                    if(k==idx2){
-                        element2=nums2[j];
-                    }
-                    j++;
+        if(nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        int low = 0, high = m;
+        while(low <= high) {
+            
+            int Px = low + (high-low)/2;
+            int Py = (m+n+1)/2 - Px;
+            
+            int x1  = (Px == 0) ? INT_MIN : nums1[Px-1];
+            int x3 = (Px == m) ? INT_MAX : nums1[Px];
+            
+            int x2  = (Py == 0) ? INT_MIN : nums2[Py-1];
+            int x4 = (Py == n) ? INT_MAX : nums2[Py];
+            
+            if(x1 <= x4 && x2 <= x3) {
+                if((m+n)%2 == 0)
+                    return (max(x1, x2) + min(x3, x4))/2.0;
+                
+                return max(x1, x2);
+            } else if(x1 > x4) {
+                high = Px-1;
+            } else {
+                low = Px+1;
             }
-            k++;
-        }   
-
-        while(i<m){
-           if(k==idx1){
-                        element1=nums1[i];
-                    }
-                    if(k==idx2){
-                        element2=nums1[i];
-                    }
-                    i++;
-                    k++;
         }
-
-        while(j<n){
-            if(k==idx1){
-                element1=nums2[j];
-                }
-                if(k==idx2){
-                element2=nums2[j];
-                }
-                j++;
-                k++;
-        }
-   
-        if(size%2==1){
-            return element2;
-        }
-
-        return (element1+element2)/2.0;
-
+        
+        return -1;
     }
 };
