cb62ed6a7a44c395506eb699715dc0620a7e2b7f
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-09-22 16:18:17 +0530

    Time: 4 ms (10.63%), Space: 7.2 MB (7.06%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,16 +4,29 @@ public:
         int m=s.length();
         int n=t.length();
 
-        int i=0;
-        int j=0;
+        map<char,vector<int>>mp;
 
-        while(i<m && j<n){
-                if(s[i]==t[j]){
-                    i++;
-                }
-                j++;
+        for(int i=0;i<n; i++){
+            mp[t[i]].push_back(i);
         }
+        int prev=-1;
 
-        return i==m;
+        for(int i=0;i<m;i++){
+            char ch=s[i];
+
+            if(mp.find(ch)==mp.end()){
+                return false;
+            }
+            vector<int> indices=mp[ch];
+
+            auto it=upper_bound(begin(indices),end(indices),prev);
+
+            if(it==indices.end()){
+                return false;
+            }
+
+            prev=*it;
+        }
+        return true;
     }
 };
