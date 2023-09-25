b37880a70d5b83eb95c2f1b345414a8b8dd0df16
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-09-25 15:48:03 +0530

    Time: 0 ms (100%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sum_s=0;
-        for(char &ch :s){
-            sum_s+=ch;
-        }
-        int sum_t=0;
-        for(char &ch :t){
-            sum_t +=ch;
-        }
+       int XOR=0;
 
-        return char(sum_t-sum_s);
+       for(char &ch:s){
+           XOR^=ch;
+       }
+
+       for(char &ch:t){
+           XOR^=ch;
+       }
+
+       return char(XOR);
     }
 };
