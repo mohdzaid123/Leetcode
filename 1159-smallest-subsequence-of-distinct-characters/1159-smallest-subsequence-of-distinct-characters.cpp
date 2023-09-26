945f6283485a5bf60686aaed817af6df5542de14
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-09-26 17:27:19 +0530

    Time: 0 ms (100%), Space: 6.6 MB (27.49%) - LeetHub

--- 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
@@ -3,39 +3,34 @@ public:
     string smallestSubsequence(string s) {
         int n=s.length();
 
-       string result;
+        string result;
+        vector<bool> taken(26,false);
+        vector<int> lastIndex(26);
 
-       vector<bool> taken(26,false);
-       vector<int> lastIndex(26);
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+             lastIndex[ch-'a']=i;
+        }
 
-       for(int i=0;i<n;i++){
-           char ch=s[i];
-
-           lastIndex[ch-'a']=i;
-
-           
-       }
-
-       for(int i=0;i<n;i++){
-           char ch=s[i];
-
+        for(int i=0;i<n;i++){
+            char ch=s[i];
            int idx=ch-'a';
 
-           if(taken[idx]==true)
-           {
+           if(taken[idx]==true){
                continue;
            }
 
            while(result.length()>0 && result.back()>ch && lastIndex[result.back()-'a']>i){
-               taken[result.back()-'a']=false;
+             taken[result.back()-'a']=false;
                result.pop_back();
+
+
            }
            result.push_back(ch);
            taken[idx]=true;
 
+        }
+        return result;
 
-       }
-
-       return result;
     }
 };
