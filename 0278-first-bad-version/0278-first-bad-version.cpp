012488cdeb4f01855633718b147101b15ffead6f
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-10-13 15:14:30 +0530

    Time: 0 ms (100%), Space: 6.3 MB (15.36%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long int start = 0 ; 
+        long long int end = n ; 
+        long long int minBadVersion ;
+         
+        while (start <= end)
+        {
+            long long int mid = (start + end) / 2 ; 
+
+            if (isBadVersion(mid) == 1)
+            {
+                minBadVersion = mid ; 
+                end = mid - 1 ; 
+            }
+            else
+            {
+                start = mid + 1 ; 
+            }
+        }
+        return minBadVersion ;
+    }
+};
