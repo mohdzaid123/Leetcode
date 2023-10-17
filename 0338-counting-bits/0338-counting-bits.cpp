08544f7f7f9d86164b60edc6f75cfd23f82cf78d
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-10-17 22:44:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (49.63%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,8 +3,17 @@ public:
     vector<int> countBits(int n) {
         vector<int> result(n+1);
 
+        if(n==0){
+           return result; 
+        }
+        result[0]=0;
+
         for(int i=0;i<=n;i++){
-            result[i]=__builtin_popcount(i); 
+           if(i%2!=0){
+               result[i]=result[i/2]+1;
+           }else{
+               result[i]=result[i/2];
+           }
         }
 
         return result;
