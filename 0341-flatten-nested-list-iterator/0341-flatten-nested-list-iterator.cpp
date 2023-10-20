5632b1742c58434ec33516f8eae170ae6bbfbd09
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-10-20 21:46:11 +0530

    Time: 3 ms (94.83%), Space: 13.6 MB (67.35%) - LeetHub

--- 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
@@ -18,16 +18,16 @@
 
 class NestedIterator {
 public:
-stack<NestedInteger> st;
+stack<NestedInteger*> st;
     NestedIterator(vector<NestedInteger> &nestedList) {
         int n=nestedList.size();
         for(int i=n-1;i>=0;i--){
-            st.push(nestedList[i]);
+            st.push(&nestedList[i]);
         }
     }
     
     int next() {
-        int num=st.top().getInteger();
+        int num=st.top()->getInteger();
 
         st.pop();
         return num;
@@ -39,15 +39,15 @@ stack<NestedInteger> st;
             return false;
         }
         while(!st.empty()){
-            NestedInteger curr=st.top();
-            if(curr.isInteger())
+            NestedInteger* curr=st.top();
+            if(curr->isInteger())
             {
                 return true;
             }
             st.pop();
-            vector<NestedInteger> vec=curr.getList();
+            vector<NestedInteger>& vec=curr->getList();
             for(int i=vec.size()-1;i>=0;i--){
-                st.push(vec[i]);
+                st.push(&vec[i]);
             }
         }
         return false;
