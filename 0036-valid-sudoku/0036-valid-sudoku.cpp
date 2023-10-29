82bba002b5f286d3b4318e0987a0734b2fc32267
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-10-29 22:58:15 +0530

    Time: 18 ms (58.97%), Space: 20.3 MB (35.33%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,74 +1,29 @@
 class Solution {
 public:
-bool validBox(vector<vector<char>>& board,int sr,int er,int sc,int ec){
-    // 3*3
-    unordered_set<char> st;
-    for(int i=sr;i<=er;i++){
-        for(int j=sc;j<=ec;j++){
-            if(board[i][j]=='.'){
-                continue;
-            }
-            if(st.find(board[i][j])!=st.end()){
-                return false;
-            }
-
-            st.insert(board[i][j]);
-        }
-    }
-    return true;
-}
     bool isValidSudoku(vector<vector<char>>& board) {
-            // row=9
-            // col=9
+        unordered_set<string> st;
 
-            // validate
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
 
-            for(int row=0;row<9;row++){
-                unordered_set<char> st;
-                for(int col=0;col<9;col++){
-                    if(board[row][col]=='.'){
-                        continue;
-                    }
+                string row=string(1,board[i][j])+"_ROW_"+to_string(i);
+                string col=string(1,board[i][j])+"_COL_"+to_string(j);
+                string box=string(1,board[i][j])+"_COL_"+to_string(i/3)+'_'+to_string(j/3);
 
-                    if(st.find(board[row][col])!=st.end()){
-                        return false;
-                    }
-
-                    st.insert(board[row][col]);
+                if(st.find(row)!=st.end()||st.find(col)!=st.end()||st.find(box)!=st.end()){
+                    return false;
                 }
-            }
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
 
-            for(int col=0;col<9;col++){
-                unordered_set<char> st;
 
-                for(int row=0;row<9;row++){
-                    if(board[row][col]=='.'){
-                        continue;
-                    }
 
-                    if(st.find(board[row][col])!=st.end()){
-                        return false;
-                    }
-
-                    st.insert(board[row][col]);
-                }
-            }
-
-            // validate 3*3
-            for(int sr=0;sr<9;sr+=3){
-                int er=sr+2;
-
-                for(int sc=0;sc<9;sc+=3){
-                    int ec=sc+2;
-
-                    // sr,er,sc,ec
-
-                    if(!validBox(board,sr,er,sc,ec)){
-                            return false;
-                    }
-                }
 
             }
-            return true;
+           
+        }
+         return true;
     }
 };
