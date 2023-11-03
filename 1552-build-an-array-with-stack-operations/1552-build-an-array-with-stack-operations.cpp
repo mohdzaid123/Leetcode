1b15d13da4a4ccabf546db76fdab03b3b90e5103
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-11-03 16:06:28 +0530

    Time: 4 ms (39%), Space: 8.2 MB (22.92%) - LeetHub

---
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int stream=1;
+        vector<string> result;
+
+        int i=0;
+        while(i<target.size()&& stream<=n){
+            result.push_back("Push");
+
+            if(target[i]==stream){
+                    i++;
+            }else{
+                result.push_back("Pop");
+            }
+            stream++;
+        }
+        return result;
+    }
+};
