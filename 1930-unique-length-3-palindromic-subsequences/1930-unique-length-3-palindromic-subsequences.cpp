eb8a1e45d9bedc1e3f677041164a12a0ca5d07e5
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-11-14 14:02:04 +0530

    Time: 245 ms (78.31%), Space: 13.4 MB (65.98%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -3,25 +3,45 @@ public:
     int countPalindromicSubsequence(string s) {
         int n=s.length();
         
-        unordered_set<char> letters;
+        vector<pair<int,int>>indices(26,{-1,-1});
+        
         for(int i=0;i<n;i++){
-            letters.insert(s[i]);
+            char ch=s[i];
+            int idx=ch-'a';
+            
+            if(indices[idx].first==-1){
+                indices[idx].first=i;
+            }
+            indices[idx].second=i;
         }
+        // unordered_set<char> letters;
+        // for(int i=0;i<n;i++){
+        //     letters.insert(s[i]);
+        // }
+        
+        
         
         int result=0;
-        for(char letter:letters){
-            int left_idx=-1;
-            int right_idx=-1;
+         for(int i=0;i<26;i++){
+             
+             int left_idx=indices[i].first;
+             int right_idx=indices[i].second;
+             
+             if(left_idx==-1){
+                 continue;
+             }
+//             int left_idx=-1;
+//             int right_idx=-1;
             
-            for(int i=0;i<n;i++){
-                if(s[i]==letter){
-                    if(left_idx==-1){
-                        left_idx=i;
-                    }
+//             for(int i=0;i<n;i++){//
+//                 if(s[i]==letter){
+//                     if(left_idx==-1){
+//                         left_idx=i;
+//                     }
                     
-                    right_idx=i;
-                }
-            }
+//                     right_idx=i;
+//                 }
+          // }
             
             unordered_set<char>st;
             for(int middle=left_idx+1;middle<=right_idx-1;middle++){
