f96ebbd558a6fbd27adb3ebf779d1e7f76be515d
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-11-18 16:35:56 +0530

    Time: 176 ms (38.78%), Space: 99.3 MB (85.20%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
-    
-    int findBestBinary(int target_idx, int k, vector<int>& nums, vector<long>& prefixSum) {
-        int target = nums[target_idx];
-        
-        int i = 0;
-        int j = target_idx;
-        int result = target_idx;
-        
-        while(i <= j) {
-            int mid = i + (j-i)/2;
-            
-            long count      = (target_idx - mid + 1);
-            long windowSum  = (count * target);
-            long currSum    = prefixSum[target_idx] - prefixSum[mid] + nums[mid];
-            
-            int ops      = windowSum - currSum;
-            
-            if(ops > k) {
-                i = mid+1;
-            } else {
-                result = mid;
-                j = mid-1;
-            }
-        }
-        
-        return target_idx-result+1;
-    }
-    
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
+        sort(begin(nums),end(nums));
         
-        sort(begin(nums), end(nums));
-        vector<long> prefixSum(n);
-        prefixSum[0] = nums[0];
+        int n=nums.size();
         
-        for(int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i-1] + nums[i];
+        int result=0;
+        int l=0;
+        long currSum=0;
+        
+        for(int r=0;r<n;r++){
+            long target=nums[r];
+            
+            currSum+=nums[r];
+            
+            // int ops=;
+            while((r-l+1)*target-currSum>k){
+                currSum-=nums[l];
+                l++;
+            }
+            result=max(result,r-l+1);
+            
         }
-        
-        int result = 0;
-        
-        for(int i = 0; i<n; i++) {
-            result = max(result, findBestBinary(i, k, nums, prefixSum));
-        }
-        
         return result;
+        
+        
     }
 };
