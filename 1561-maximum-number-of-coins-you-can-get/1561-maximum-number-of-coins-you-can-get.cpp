ba634a060b0f7f2305fa3b66a4b021108d339ff9
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-11-24 15:33:31 +0530

    Time: 83 ms (89.86%), Space: 53.8 MB (54.72%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -7,16 +7,13 @@ public:
         
         int result=0;
         
-        int Bob=0;
-        int M=n-2;
-        int Alice=n-1;
+      
+      
         
-        while(M>Bob){
-            result+=piles[M];
-            M-=2;
-            Bob++;
-            Alice-=2;
-        }
+      for(int M=n/3;M<n;M+=2){
+          result+=piles[M];
+          
+      }
         return result;
     }
 };
