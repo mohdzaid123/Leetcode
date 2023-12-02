2bdf83cda4316fc37f7fadb0e10465e9db193a76
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-12-02 14:51:09 +0530

    Time: 50 ms (66.92%), Space: 20.5 MB (71.98%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+//         freaquency of characters
+        vector<int>charCount(26,0);
+        
+        for(char &ch:chars){
+            charCount[ch-'a']++;
+        }
+        
+        int result=0;
+        for(string &word:words){
+            vector<int>wordCount(26,0);
+            for(char &ch:word){
+                wordCount[ch-'a']++;
+            }
+            bool ok=true;
+            for(int i=0;i<26;i++){
+                if(wordCount[i]>charCount[i]){
+                    ok=false;
+                    break;
+                }
+            }
+            
+            if(ok==true){
+                result+=word.length();
+            }
+            
+        }
+        return result; 
+    }
+};
