eebc7d39626c4fe63fb5ea294330ce4b53116971
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-12-10 04:08:11 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (29.66%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,56 @@
 class Solution {
 public:
+    void lpsfind(vector<int> &lps,string s ) {
+	    
+	    int pre=0,suf=1;
+	    
+	    while(suf<s.size())
+	    {
+	        if(s[pre]==s[suf]){
+	            lps[suf]=pre+1;
+	            suf++;
+	            pre++;
+	        }else{
+	            if(pre==0){
+	                lps[suf]=0;
+	                suf++;
+	            }else{
+	                pre=lps[pre-1];
+	            }
+	        }
+	        
+	    }
+	    
+	  
+	}
     int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m=needle.size();
-        for(int i=0;i<=n-m;i++){
-            int first=i ,second=0;
-            while(second<m){
-                if(haystack[first]!=needle[second]){
-                    break;
-                }else{
-                     first++;
+        vector<int>lps(needle.size(),0);
+        lpsfind(lps,needle);
+        
+        int first=0,second=0;
+        
+        while(first<haystack.size()&&second<needle.size()){
+//             match
+//             not matched
+            if(haystack[first]==needle[second]){
+                first++;
                 second++;
+                
+            }else{
+                if(second==0)
+                {
+                    first++;
+                }else{
+                    second=lps[second-1];
                 }
-                if(m==second){
-                    return first-second;
-                }
-               
             }
-          
+            
         }
-          return -1;
+        
+        if(second==needle.size()){
+            return first-second;
         }
+        return -1;
+        
+    }
 };
