096378ed9d91f01f7bc9ba4c5377ed708d209d69
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-12-11 19:05:02 +0530

    Time: 8 ms (78.76%), Space: 13 MB (46.13%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,14 +1,66 @@
 class Solution {
 public:
-    int findSpecialInteger(vector<int>& arr) {
-        int n=arr.size();
-        int freq=n/4;
+    
+    int firstOccur(int target, int l, int r, vector<int>& arr) {
         
-    for(int i=0;i<n-freq;i++){
-        if(arr[i]==arr[i+freq]){
-            return arr[i];
+        int result = -1;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(arr[mid] == target) {
+                result = mid;
+                r = mid-1;
+            } else if(arr[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
         }
+        
+        return result;
+        
     }
+    
+    int lastOccur(int target, int l, int r, vector<int>& arr) {
+        
+        int result = -1;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(arr[mid] == target) {
+                result = mid;
+                l = mid+1;
+            } else if(arr[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        
+        return result;
+        
+    }
+    
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        
+        int freq = n/4;
+        
+        vector<int> candidates{arr[n/4], arr[n/2], arr[3*n/4]};
+        
+        for(int &candidate : candidates) {
+            
+            int left = firstOccur(candidate, 0, n-1, arr);
+            int right = lastOccur(candidate, 0, n-1, arr);
+            
+            
+            if(right-left+1 > freq) {
+                return candidate;
+            }
+        }
+        
         return -1;
         
     }
