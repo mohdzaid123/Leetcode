7942f0055a3bc157dc20d8af9f0c2c1e913a82d2
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-12-14 02:12:39 +0530

    Time: 16 ms (68.27%), Space: 13.2 MB (46.93%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
+     int result=0;
+        int m=mat.size();
+        int n=mat[0].size();
         
-        vector<int> rowCount(m, 0);
-        vector<int> colCount(n, 0);
-
-        // Count the number of 1s in each row and each column
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (mat[i][j] == 1) {
-                    rowCount[i]++;
-                    colCount[j]++;
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(mat[row][col]==0){
+                    continue;
                 }
+                
+                bool special=true;
+              //                 check column
+                for(int r=0;r<m;r++){
+                    if(r!=row && mat[r][col]==1){
+                        special=false;
+                        break;
+                    }
+                }
+                
+                for(int c=0;c<n;c++){
+                    if(c!=col && mat[row][c]==1){
+                         special=false;
+                        break;
+                    }
+                }
+                if(special==true){
+                    result++;
+                }
+                
+                
             }
         }
-
-        // Count the number of special positions
-        int specialCount = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
-                    specialCount++;
-                }
-            }
-        }
-
-        return specialCount;
+        return result;
     }
 };
