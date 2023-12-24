319229b402c909959c6ba470f308cc1129791513
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2023-12-24 14:58:47 +0530

    Time: 3 ms (78.34%), Space: 7.5 MB (12.76%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,24 +4,21 @@ public:
         int n=s.length();
         
         int start_with_0=0; //01010101
-        int start_with_1=0; //10101010
+        //10101010
       
         
         for(int i=0;i<n;i++){
             if(i%2==0){
-                if(s[i]=='0'){
-                    start_with_1++;
-                }else{
+                if(s[i]=='1'){
                     start_with_0++;
                 }
             }else {
-                if(s[i]=='1'){
-                     start_with_1++;
-                }else{
-                    start_with_0++;
+                if(s[i]=='0'){
+                     start_with_0++;
                 }
             }
         }
+         int start_with_1=n-start_with_0;
         return min(start_with_0,start_with_1);
     }
 };
