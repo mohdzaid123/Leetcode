ec224c151f49f3724581d5856096b1c155a9f1e9
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-01-08 04:57:28 +0530

    Time: 4 ms (62.54%), Space: 14.2 MB (39.62%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -29,6 +29,22 @@ public:
         dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
            return dp[n]; 
     }
+    
+    int solve2(vector<int>&cost,int n){
+//         base case
+//         step 1 create dp array
+        vector<int> dp(n+1);
+        // base case analyse
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        
+//         step 3
+        for(int i=2 ;i<n;i++){
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        
+       return min(dp[n-1],dp[n-2]);
+    }
     int minCostClimbingStairs(vector<int>& cost) {
         // int n=cost.size();
         // // can be simlified
@@ -36,9 +52,9 @@ public:
         // return ans;
         int n=cost.size();
         // step 1
-        vector<int>dp(n+1,-1);
-        int ans=min(solve(cost,n-1,dp),solve(cost,n-2,dp));
-        return ans;
+        // vector<int>dp(n+1,-1);
+        // int ans=min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        return solve2(cost,n);
         
     }
 };
