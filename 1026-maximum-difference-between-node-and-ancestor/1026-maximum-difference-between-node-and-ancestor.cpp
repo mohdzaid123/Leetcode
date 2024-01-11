f99d770345313cba9e62c550fcfcb6e8cfa38b5e
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-01-11 14:03:26 +0530

    Time: 3 ms (85.41%), Space: 10.1 MB (59.95%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int maxDiffi;
+
+// void findMaxDiffUtil(TreeNode* root,TreeNode*child){
+//     if(root==NULL||child==NULL){
+//         return;
+//     }
+//     maxDiffi=max(maxDiffi,abs(root->val - child->val));
+//     findMaxDiffUtil(root,child->left);
+//     findMaxDiffUtil(root,child->right);
+// }
+
+// void findMaxDiff(TreeNode* root){
+//     if(root==NULL){
+//         return;
+//     }
+//     findMaxDiffUtil(root,root->left);
+//     findMaxDiffUtil(root,root->right);
+    
+//     findMaxDiff(root->left);
+//     findMaxDiff(root->right);
+    
+// }
+int findMaxDiff(TreeNode* root,int minV,int maxV){
+if(root==NULL){
+    return abs(minV-maxV);
+}
+    minV=min(minV,root->val);
+    maxV=max(maxV,root->val);
+
+    int l=findMaxDiff(root->left,minV,maxV);
+    int r=findMaxDiff(root->right,minV,maxV);
+
+    return max(l,r);
+}
+    int maxAncestorDiff(TreeNode* root) {
+        return findMaxDiff(root,root->val,root->val);
+    
+    }
+};
