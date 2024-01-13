bab341c99d30b7d0fbcfba52f5e181318b530890
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-01-13 15:15:11 +0530

    Time: 45 ms (88.87%), Space: 17.1 MB (36.03%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -3,19 +3,25 @@ public:
     int minSteps(string s, string t) {
         int n=s.length();
         
-        int mp_s[26]={0};
-        int mp_t[26]={0};
+        int mp[26]={0};
+        // int mp_s[26]={0};
+        // int mp_t[26]={0};
         
         for(int i=0;i<n;i++){
-            mp_s[s[i]-'a']++;
-            mp_t[t[i]-'a']++;
+            // mp_s[s[i]-'a']++;
+            mp[s[i]-'a']++;
+            mp[t[i]-'a']--;
+            // mp_t[t[i]-'a']++;
         }
         
         int result=0;
         
         for(int i=0;i<26;i++){
-            if(mp_s[i]>mp_t[i])
-            result+=mp_s[i]-mp_t[i];
+            if(mp[i]>0){
+                result+=mp[i];
+            }
+            // if(mp_s[i]>mp_t[i])
+            // result+=mp_s[i]-mp_t[i];
         }
         return result;
     }
