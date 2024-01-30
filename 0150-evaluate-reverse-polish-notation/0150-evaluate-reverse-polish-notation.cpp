75f8a419b0b05f51fa586b4f8848d998c8ecbba0
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-01-30 13:09:52 +0530

    Time: 8 ms (70.52%), Space: 15.5 MB (5.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-int Operate(int a ,int b,string token){
-    if(token=="+"){
-        return a+b;
-    }
-    if(token=="*"){
-        return (long)a*(long)b;
-    }
-    if(token=="-"){
-        return a-b;
-    }
-    if(token=="/"){
-        return a/b;
-    }
-    return -1;
-}
+
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
+
+        unordered_map<string,function<int(int,int)>> mp={
+            {"+",[](int a,int b){return a+b;}},
+            {"-",[](int a,int b){return a-b;}},
+            {"*",[](int a,int b){return (long)a*(long)b;}},
+            {"/",[](int a,int b){return a/b;}},
+        };
         for(string&token:tokens){
             if(token=="*"||token=="+"||token=="-"||token=="/"){
                 // top 2 element ko  niklana operate karna hai
@@ -26,7 +19,7 @@ int Operate(int a ,int b,string token){
                 int a=st.top();
                 st.pop();
 
-                int result=Operate(a,b,token);
+                int result=mp[token](a,b);
 
                 st.push(result);
 
@@ -37,3 +30,5 @@ int Operate(int a ,int b,string token){
         return st.top();
     }
 };
+
+ 
