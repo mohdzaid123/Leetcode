d36fec9e9e02b4b8b90689977979dd7bd78763e8
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-03-11 12:15:23 +0530

    Time: 4 ms (26.39%), Space: 8.4 MB (23.18%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        map<char,int> mp;
+        string ans="";
+        for(auto x:s){
+                mp[x]++;
+        }
+        for(auto x:order){
+            if(mp.find(x)!=mp.end()){
+                auto temp=mp.find(x);
+                int count=temp->second;
+                // c->5
+                string s(count,x);
+                ans+=s;
+                mp.erase(x);
+            }
+        }
+        for(auto x:mp){
+                string s(x.second,x.first);
+                ans+=s;
+        }   
+        return ans;
+    }
+};
