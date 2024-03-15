d0ab4a764533c9f8288364796620b360cbe9a935
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-03-15 15:18:33 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (30.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -17,6 +17,6 @@ public:
             }
         }
         return st.empty(); // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-                           // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+                           // so the string is valid, otherwise, there are unmatched opening brackets, so return false flase
     }
 };
