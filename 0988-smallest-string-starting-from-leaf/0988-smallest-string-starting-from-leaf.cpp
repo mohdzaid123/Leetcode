8183b1519e0dd0f6ed3e784c0b6410f97a01b651
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-04-17 14:53:47 +0530

    Time: 7 ms (80.26%), Space: 20.2 MB (78.11%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,31 +11,29 @@
  */
 class Solution {
 public:
-    string result="";
-    
-    void dfs(TreeNode* root,string curr){
-        if(!root){
-            return;
-            
-        }
-        
-        curr=char(root->val+'a')+curr;
-        
-        
-        if(!root->left && !root->right){
-            if(result==""||result>curr){
-                result=curr;
-            }
-            return;
-        }
-        
-        dfs(root->left,curr);
-        dfs(root->right,curr);
-    }
     string smallestFromLeaf(TreeNode* root) {
-        dfs(root,"");
+        queue<pair<TreeNode*, string>>que;
         
-        
+        que.push({root,string(1,char(root->val+'a'))});
+        string result="";
+        while(!que.empty()){
+            auto[node,curr]=que.front();
+            
+            que.pop();
+            
+            if(!node->left&&!node->right){
+                if(result==""||result>curr){
+                    result=curr;
+                }
+            }
+            if(node->left){
+                que.push({node->left,char(node->left->val+'a')+curr});
+            }
+            
+             if(node->right){
+                que.push({node->right,char(node->right->val+'a')+curr});
+            }
+        }
         return result;
     }
 };
