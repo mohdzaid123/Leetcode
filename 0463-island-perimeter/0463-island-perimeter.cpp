67c1040b42ccef0b2902468e45b11ad3c2bba794
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-04-18 15:40:25 +0530

    Time: 99 ms (11.33%), Space: 108.2 MB (7.59%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -2,37 +2,57 @@ class Solution {
 public:
     int m;
     int n;
-    int peri;
+
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     
-    void dfs(vector<vector<int>>& grid,int i,int j){
-        if(i<0||i>=m||j<0||j>=n||grid[i][j]==0){
-            peri++;
-            return;
-        }
-        if(grid[i][j]==-1){
-            return; 
-        }
-        grid[i][j]=-1;   //mark visited
+    int bfs(vector<vector<int>>& grid, int i, int j) {
+        int perim = 0;
         
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        grid[i][j] = -1;
+        
+        while(!que.empty()) {
+            
+            auto it = que.front();
+            que.pop();
+            
+            //it.first = i
+            //it.second = j
+            
+            for(auto &dir : directions) {
+                int i_ = it.first + dir[0];
+                int j_ = it.second + dir[1];
+                
+                if(i_ < 0 || i_ >= m || j_ < 0 || j_ >= n || grid[i_][j_] == 0)
+                    perim++;
+                else if(grid[i_][j_] == -1) {
+                    continue;
+                } else {
+                    que.push({i_, j_});
+                    grid[i_][j_] = -1;
+                }
+            }
+            
+        }
+        
+        return perim;
     }
+    
     int islandPerimeter(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        peri=0;
+        m    = grid.size();
+        n    = grid[0].size();
         
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    dfs(grid,i,j);
-                return peri;
-                    }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                
+                if(grid[i][j] == 1) {
+                    return bfs(grid, i, j);
+                }
+                
             }
         }
+        
         return -1;
     }
 };
