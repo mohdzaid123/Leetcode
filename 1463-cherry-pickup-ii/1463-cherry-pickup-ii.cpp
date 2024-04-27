7323328a062cf6b69ba23404dac7e53606b9336b
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-04-27 15:56:06 +0530

    Time: 11 ms (97.84%), Space: 12.4 MB (89.07%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int m,n;
-    int t[71][71][71];
-    int solve(vector<vector<int>>& grid,int row,int c1,int c2){
-        if(row>=m){  //out of bound
-            return 0;
-        }
-        if(t[row][c1][c2]!=-1){
-            return t[row][c1][c2];
-        }
-        int cherry=grid[row][c1];
-        if(c1!=c2){ //same cell can be taken only once
-            cherry+=grid[row][c2];
-        }
-        int ans=0;
-        for(int i=-1;i<=1;i++){ //R1
-            for(int j=-1;j<=1;j++){ //R2
-                int newRow=row+1;
-                int new_c1 =c1+i;
-                int new_c2=c2+j;
-                
-                if(new_c1>=0&&new_c1<n&&new_c2>=0&&new_c2<n){
-                    ans=max(ans,solve(grid,newRow,new_c1,new_c2));
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        int t[71][71][71]={0};
+      //t row[c1][c2]=max cherries collected till now,c1 by robot  
+        t[0][0][n-1]=(n==1)?grid[0][0]:grid[0][0]+grid[0][n-1];
+        
+        for(int row=1;row<m;row++){
+            for(int c1=0;c1<=min(n-1,row);c1++){
+                for(int c2=max(0,n-row-1);c2<n;c2++){
+                    int prevRowMax=0;
+                    for(int prevCol1=max(c1-1,0);prevCol1<=min(n-1,c1+1);prevCol1++){
+                        for(int prevCol2=max(c2-1,0);prevCol2<=min(n-1,c2+1);prevCol2++){
+                        prevRowMax=max(prevRowMax,t[row-1][prevCol1][prevCol2]);
+                    }
+                    }
+                    if(c1==c2){
+                        t[row][c1][c2]=prevRowMax+grid[row][c1];
+                    }else{
+                        t[row][c1][c2]=prevRowMax+grid[row][c1]+grid[row][c2];
+                    }
+                    
                 }
-                
             }
         }
-        return t[row][c1][c2]= cherry+ans;
-    }
-        
-    int cherryPickup(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        memset(t,-1,sizeof(t));
-        return solve(grid,0,0,n-1);
+        int result=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                result=max(result,t[m-1][i][j]);
+            }
+        }
+        return result;
     }
 };
