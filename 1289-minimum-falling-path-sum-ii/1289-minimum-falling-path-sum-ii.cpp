3462bb3f550bed4a201dad55069fab81e31a5027
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-04-28 20:14:27 +0530

    Time: 27 ms (77.21%), Space: 18.2 MB (50.68%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -4,25 +4,42 @@ public:
         int n=grid.size();
         vector<vector<int>>t(n,vector<int>(n,INT_MAX));
         
+        int nextMinCol1=-1;
+        int nextMinCol2=-1;
+        
         for(int col=0;col<n;col++){
             t[n-1][col]=grid[n-1][col];
-        }
-        for(int row=n-2;row>=0;row--){
-            for(int col=0;col<n;col++){
-                int ans=INT_MAX;
-                for(int nextCol=0;nextCol<n;nextCol++){
-                    if(nextCol!=col){
-                        ans=min(ans,t[row+1][nextCol]);
-                    }
-                }
-                t[row][col]=grid[row][col]+ans;
+            
+            if(nextMinCol1==-1||t[n-1][col]<=t[n-1][nextMinCol1]){
+                nextMinCol2= nextMinCol1;
+                nextMinCol1=col;
+            }else if(nextMinCol2==-1||t[n-1][col]<=t[n-1][nextMinCol2]){
+                nextMinCol2=col;
             }
         }
-        int result=INT_MAX;
-        for(int col=0;col<n;col++){
-            result=min(result,t[0][col]);
+        for(int row=n-2;row>=0;row--){
+            int minCol1=-1;
+            int minCol2=-1;
+            for(int col=0;col<n;col++){
+                
+           
+               if(col != nextMinCol1){
+                   t[row][col]=grid[row][col]+t[row+1][nextMinCol1];
+               }else{
+                   t[row][col]=grid[row][col]+t[row+1][nextMinCol2];
+               }
+                if(minCol1==-1||t[row][col]<=t[row][minCol1]){
+                    minCol2=minCol1;
+                    minCol1=col;
+                }else if(minCol2==-1||t[row][col]<=t[row][minCol2]){
+                    minCol2=col;
+                }
+            }
+            nextMinCol1=minCol1;
+            nextMinCol2=minCol2;
         }
-        return result;
+        
+        return t[0][nextMinCol1];
         
     }
 };
