a1791af89964eba3d71c83e0026c9f48f2f25a24
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-05-27 14:49:40 +0530

    Time: 5 ms (33.11%), Space: 10 MB (85.84%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i <= n; ++i) {
+            int count = 0;
+            for (int j = 0; j < n; ++j) {
+                if (nums[j] >= i)
+                    count++;
+            }
+            if (count == i)
+                return i;
+        }
+        return -1;
+    }
+};
