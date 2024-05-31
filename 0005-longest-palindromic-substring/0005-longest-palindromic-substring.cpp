2bd64996cb6cebb23d7f6f8dace7d2618563c576
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-05-31 15:37:42 +0530

    Time: 856 ms (11.53%), Space: 8.1 MB (93.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -16,7 +16,7 @@ public:
         int sp=0;
         
         for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
+            for(int j=i;j<n;j++){
                 if(solve(s,i,j)==true){
                     if(j-i+1>maxLen){
                         maxLen=j-i+1;
