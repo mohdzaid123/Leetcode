a601685689100a7c70ee5e1ee5b7fa06dc982c62
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-05-31 15:37:21 +0530

    Time: 873 ms (10.10%), Space: 8.1 MB (93.35%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(string &s,int i,int j){
+        if(i>=j){
+            return true;
+        }
+        if(s[i]==s[j]){
+            return solve(s,i+1,j-1);
+        }
+        return false;
+    } 
+    string longestPalindrome(string s) {
+        int n=s.length();
+        
+        int maxLen=INT_MIN;
+        int sp=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(solve(s,i,j)==true){
+                    if(j-i+1>maxLen){
+                        maxLen=j-i+1;
+                        sp=i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp,maxLen);
+    }
+};
