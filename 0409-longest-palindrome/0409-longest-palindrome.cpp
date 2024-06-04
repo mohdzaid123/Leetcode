83bc3c3fb17490ce4616e059abe8b3038fbf72a7
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-06-04 13:01:48 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (62.59%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -4,24 +4,35 @@ public:
      int n=s.length();
       unordered_map<char,int>mp;  
         
-        for(char ch:s){
-            mp[ch]++;
-        }
+        // for(char ch:s){
+        //     mp[ch]++;
+        // }
         
         int result=0;
-        bool oddFreq=false;
+        int oddFreq=0;
+        // bool oddFreq=false;
         
-        for(auto& it:mp ){
-            if(it.second%2==0){
-                result+=it.second;
+        // for(auto& it:mp ){
+        //     if(it.second%2==0){
+        //         result+=it.second;
+        //     }else{
+        //         result+=it.second-1;
+        //         oddFreq=true;
+        //     }
+        // }
+        
+        for(char &ch:s){
+            mp[ch]++;
+            
+            if(mp[ch]%2!=0){
+                oddFreq++;
             }else{
-                result+=it.second-1;
-                oddFreq=true;
+                oddFreq--;
             }
         }
-        if(oddFreq){
-            result++;
+        if(oddFreq>0){
+          return   n-oddFreq+1;
         }
-        return result;
+        return n;
     }
 };
