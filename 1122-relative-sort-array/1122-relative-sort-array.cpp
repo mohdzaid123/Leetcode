f9e29b850d74e944fa086e716ecd3883fa3edb9d
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-06-11 21:12:55 +0530

    Time: 5 ms (34.15%), Space: 10.2 MB (41.93%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-       vector<int> ans;
-       map<int,int> mp;
-
-       for(auto x:arr1){
-           mp[x]++;
-
-       } 
-       for(auto temp:arr2){
-           if(mp.find(temp)!=mp.end()){
-               auto x=mp.find(temp);
-               int count=x->second;
-               vector<int> v(count,temp);  //short hand //22 333 count value 
-               ans.insert(ans.end(),v.begin(),v.end());
-               mp.erase(temp);
-           }
-       }
-
-       for(auto x:mp){
-           int ele=x.first;
-           int count=x.second;
-           vector<int> v(count,ele);
-           ans.insert(ans.end(),v.begin(),v.end());
-       }
-       return ans;
+        map<int,int>mp;
+        
+        for(int &num:arr1){
+            mp[num]++;
+        }
+        
+        int i=0;
+        for(int &num:arr2){
+            while(mp[num]-->0){
+                arr1[i++]=num;
+            }
+        }
+        
+        for(auto &it:mp){
+            int freq=it.second;
+            while(freq>0){
+                arr1[i++]=it.first;
+                freq--;
+            }
+        }
+        
+        return arr1;
     }
 };
