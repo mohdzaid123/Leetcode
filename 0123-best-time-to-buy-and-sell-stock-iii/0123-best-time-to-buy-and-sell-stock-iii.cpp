5d3f2ae395bc5e701b4aae4fa6b0d1e89540c2fd
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-06-17 19:18:09 +0530

    Time: 158 ms (63.57%), Space: 78 MB (80.75%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -15,7 +15,9 @@ public:
     
     int solveTab(vector<int>& prices){
         int n=prices.size();
-         vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2,vector<int>(3,0)));
+         // vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>>curr(2,vector<int>(3,0));
+        vector<vector<int>>next(2,vector<int>(3,0));
         
         for(int index =n-1;index>=0;index--){
             for(int buy=0;buy<=1;buy++){
@@ -23,19 +25,20 @@ public:
                     int profit=0;
                     
                     if(buy){
-                        int buyKaro=-prices[index]+dp[index+1][0][limit];
-                        int skipKaro=0+dp[index+1][1][limit];
+                        int buyKaro=-prices[index]+next[0][limit];
+                        int skipKaro=0+next[1][limit];
                         profit=max(buyKaro,skipKaro);
                     }else{
-                        int sellKaro=prices[index]+dp[index+1][1][limit-1];
-                        int skipKaro=0+dp[index+1][0][limit];
+                        int sellKaro=prices[index]+next[1][limit-1];
+                        int skipKaro=0+next[0][limit];
                         profit=max(sellKaro,skipKaro);
                     }
-                    dp[index][buy][limit]=profit;
+                    curr[buy][limit]=profit;
                 }
             }
+            next=curr;
         }
-        return dp[0][1][2];
+        return next[1][2];
     }
     int maxProfit(vector<int>& prices) {
         
