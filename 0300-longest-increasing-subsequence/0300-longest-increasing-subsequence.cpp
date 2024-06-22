a5f16262028287eb618b3b7f2a5832fbec97e18e
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2024-06-22 23:53:03 +0530

    Time: 543 ms (10.36%), Space: 40.3 MB (35.70%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> sorted;
-        
-        for(int i = 0; i<n; i++) {
-            /*
-			     Why lower bound ?
-				 We want increasing subsequence and  hence 
-				 we want to eliminate the duplicates as well.
-				 lower_bound returns iterator to "next greater or equal to"
-			*/
-            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+    int n;
+    int t[2501][2501];
+    int solve(vector<int>& nums,int i,int P){
+        if(i>=n){
+            return 0;
             
-            if(it == end(sorted))
-                sorted.push_back(nums[i]); //greatest : so insert it
-            else
-                *it = nums[i]; //replace
         }
         
-        return (int)sorted.size();
+        if(P!=-1 && t[i][P]!=-1){
+            return t[i][P];
+        }
+        int take=0;
+        if(P==-1||nums[i]>nums[P]){
+            take =1+solve(nums,i+1,i);
+        }
+        
+        int skip=solve(nums,i+1,P);
+        
+         if(P!=-1){
+              t[i][P]= max(take,skip);
+         }
+        return max(take,skip);
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        n=nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(nums,0,-1);
     }
 };
