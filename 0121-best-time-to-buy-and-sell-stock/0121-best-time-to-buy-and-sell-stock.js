aa2e97ad3625e5944ded84b187f32accb76032e3
MohdZaid <48615234+mohdzaid123@users.noreply.github.com>
2026-02-19 01:48:34 +0530

    Time: 2 ms (81.98%), Space: 65.2 MB (64.72%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+    let min=prices[0];
+    let maxProfit=0;
+
+    for(let i=1;i<prices.length;i++){
+        if(prices[i]-min>maxProfit){
+            maxProfit=prices[i]-min;
+        }
+        if(prices[i]<min){
+            min=prices[i];
+        }
+    }
+    return maxProfit;
+};
